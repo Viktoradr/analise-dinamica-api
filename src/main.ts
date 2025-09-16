b654b17a24e7a13a2d649f79f3466cf1f1a144ba
Victor Alves <victor.adrodrigues@outlook.com>
2025-09-16 16:39:11 -0300

    teste

--- src/main.ts
+++ src/main.ts
@@ -1,42 +1,53 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { configSwagger } from './config/swagger';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import express from 'express';
-import cors from 'cors';
-import serverless from 'serverless-http';
+// import { ExpressAdapter } from '@nestjs/platform-express';
+// import express from 'express';
+// import cors from 'cors';
+// import serverless from 'serverless-http';
 
-const expressApp = express();
-expressApp.use(cors({
-  origin: [
-    'http://localhost:4200', // Desenvolvimento local
-    'https://analise-dinamica.vercel.app'], // Frontend autorizado
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-  credentials: true
-}));
-const adapter = new ExpressAdapter(expressApp);
+// const expressApp = express();
+// expressApp.use(cors({
+//   origin: [
+//     'http://localhost:4200', // Desenvolvimento local
+//     'https://analise-dinamica.vercel.app'], // Frontend autorizado
+//   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+//   credentials: true
+// }));
+// const adapter = new ExpressAdapter(expressApp);
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, adapter);
+  const app = await NestFactory.create(AppModule);
   
+  app.enableCors({
+    origin: [
+      'http://localhost:4200', // Desenvolvimento local
+      'https://analise-dinamica.vercel.app'], // Frontend autorizado
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true
+  });
+
   // Habilita CORS
   // Permitir CORS
-  if (process.env.NODE_ENV !== 'production') {
-    // Em desenvolvimento, permitir todas as origens
-    app.enableCors(); 
-    // Todas as rotas terão prefixo /api
-    app.setGlobalPrefix('api');
-    // Configuração do Swagger
-    configSwagger(app);
-  }
+  // if (process.env.NODE_ENV !== 'production') { 
+  //   // Todas as rotas terão prefixo /api
+  //   app.setGlobalPrefix('api');
+  //   // Configuração do Swagger
+  //   configSwagger(app);
+  // }
+  
+  app.setGlobalPrefix('api');
+  configSwagger(app);
+  
   
   const PORT = process.env.PORT || 3000;
-  if (process.env.NODE_ENV !== 'production') {
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-  }
+  // if (process.env.NODE_ENV !== 'production') {
+  //   app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  // }
 
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   await app.init();
 }
 bootstrap();
 
-export const handler = serverless(expressApp);
+// export const handler = serverless(expressApp);
