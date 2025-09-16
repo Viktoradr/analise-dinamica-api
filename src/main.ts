2c7bd52661743fe4f2bdd807522155c9c1b65de2
Victor Alves <victor.adrodrigues@outlook.com>
2025-09-16 17:04:48 -0300

    organizando arquivos e redire2cionamento

--- src/main.ts
+++ src/main.ts
@@ -3,20 +3,6 @@ import { AppModule } from './app.module';
 import { configSwagger } from './config/swagger';
 import dotenv from "dotenv";
 dotenv.config();
-// import { ExpressAdapter } from '@nestjs/platform-express';
-// import express from 'express';
-// import cors from 'cors';
-// import serverless from 'serverless-http';
-
-// const expressApp = express();
-// expressApp.use(cors({
-//   origin: [
-//     'http://localhost:4200', // Desenvolvimento local
-//     'https://analise-dinamica.vercel.app'], // Frontend autorizado
-//   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-//   credentials: true
-// }));
-// const adapter = new ExpressAdapter(expressApp);
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -29,27 +15,16 @@ async function bootstrap() {
     credentials: true
   });
 
-  // Habilita CORS
-  // Permitir CORS
-  // if (process.env.NODE_ENV !== 'production') { 
-  //   // Todas as rotas terão prefixo /api
-  //   app.setGlobalPrefix('api');
-  //   // Configuração do Swagger
-  //   configSwagger(app);
-  // }
-
   app.setGlobalPrefix('api');
-  configSwagger(app);
   
+  configSwagger(app);
+
+  // Redireciona a raiz "/" para "/api"
+  app.getHttpAdapter().get('/', (req, res) => {
+    res.redirect('/api');
+  });
   
   const PORT = process.env.PORT || 3000;
-  // if (process.env.NODE_ENV !== 'production') {
-  //   app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-  // }
-
   app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-  //await app.init();
 }
 bootstrap();
-
-// export const handler = serverless(expressApp);
