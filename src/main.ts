ba9a1be292c205c264c0ac6a9c22a581e39c999c
Victor Alves <victor.adrodrigues@outlook.com>
2025-09-16 17:27:16 -0300

    testando serveless

--- src/main.ts
+++ src/main.ts
@@ -4,17 +4,37 @@ import { configSwagger } from './config/swagger';
 import dotenv from "dotenv";
 dotenv.config();
 
+import { ExpressAdapter } from '@nestjs/platform-express';
+import express from 'express';
+import cors from 'cors';
+
+const expressApp = express();
+
+// ✅ Configure o CORS aqui
+expressApp.use(cors({
+  origin: [
+    'http://localhost:4200',
+    'https://analise-dinamica.vercel.app',
+  ],
+  methods: ['GET','POST','PUT','DELETE','PATCH','OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+}));
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
   
-  app.enableCors({
-    origin: [
-      'http://localhost:4200', // Desenvolvimento local
-      'https://analise-dinamica.vercel.app'], // Frontend autorizado
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true
-  });
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
+
+  //const app = await NestFactory.create(AppModule);
+  
+  // app.enableCors({
+  //   origin: [
+  //     'http://localhost:4200', // Desenvolvimento local
+  //     'https://analise-dinamica.vercel.app'], // Frontend autorizado
+  //   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  //   allowedHeaders: ['Content-Type', 'Authorization'],
+  //   credentials: true
+  // });
 
   app.setGlobalPrefix('api');
   
@@ -25,7 +45,8 @@ async function bootstrap() {
     res.redirect('/api');
   });
   
-  const PORT = process.env.PORT || 3000;
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  // const PORT = process.env.PORT || 3000;
+  // app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  await app.init();
 }
 bootstrap();
