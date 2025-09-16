cd86ab89fb09ea6077093e12dce4a34a6f222589
Victor Alves <victor.adrodrigues@outlook.com>
2025-09-16 17:41:09 -0300

    teste com html e usando só a origin

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,11 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return `<html>
+        <head><title>Hello</title></head>
+        <body>
+          <h1>Hello World em HTML 🚀</h1>
+        </body>
+      </html>`;
   }
 }
